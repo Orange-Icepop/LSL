255cc8f0f502394871ae46787c1b0a0ddbd553a6
Orange-Icepop <czq13482103069@hotmail.com>
2024-06-17 19:51:38 +0800

    Canary0.01.7

---
+++ LSL/Views/Bar.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LSL.Views
+{
+    public partial class Bar : UserControl
+    {
+        public Bar()
+        {
+            InitializeComponent();
+        }
+    }
+}
