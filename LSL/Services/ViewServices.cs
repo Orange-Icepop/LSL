72e1ef2a0e7750cd3a1f79daa9ce8e56385fb03f
Orange-Icepop <czq13482103069@hotmail.com>
2024-07-05 13:25:35 +0800

    backup

---
+++ LSL/Services/ViewServices.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Controls;
+using LSL.Views;
+using LSL.Views.Home;
+using LSL.Views.Server;
+using LSL.Views.Download;
+using LSL.Views.Settings;
+using ReactiveUI;
+using System;
+using System.Windows.Input;
+using System.Reactive;
+using System.Collections.Generic;
+using Avalonia.Markup.Xaml.MarkupExtensions;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using Avalonia.Interactivity;
+
+namespace LSL.Services
+{
+    public class ViewServices : ISharedService
+    {
+
+    }
+}
---
+++ LSL/ViewModels/SharedData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSL.ViewModels
+{
+    public class SharedData
+    {
+        public string OnLoadPages{ get; set; }
+    }
+}
