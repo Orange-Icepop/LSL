4b1ee3448b829b14cb83cfbf14eae6d8106c45ee
Orange-Icepop <czq13482103069@hotmail.com>
2025-01-24 10:43:56 +0800

    新增MyListItem控件 增加了第三方库使用声明

---
+++ LSL/Components/MyListItem.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LSL.Components
+{
+    public partial class MyListItem : Button
+    {
+        public MyListItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
