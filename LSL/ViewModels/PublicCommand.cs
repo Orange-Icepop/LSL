c0f60691d038e61bbc581a1406079b54d670b3a9
Orange-Icepop <czq13482103069@hotmail.com>
2025-08-21 10:35:16 +0800

    添加跨平台打开文件资源管理器方法

--- LSL/ViewModels/PublicCommand.cs
+++ LSL/ViewModels/PublicCommand.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
 using System.Reactive.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Avalonia.Threading;
 using Microsoft.Extensions.Logging;
@@ -17,7 +18,8 @@ namespace LSL.ViewModels
     {
         public PublicCommand(AppStateLayer appState, ServiceConnector serveCon) : base(appState, serveCon)
         {
-            OpenWebPageCmd = ReactiveCommand.Create<string>(OpenWebPage);// 打开网页命令-实现
+            OpenWebPageCmd = ReactiveCommand.CreateFromTask<string>(OpenWebPage);// 打开网页命令-实现
+            OpenFileCmd = ReactiveCommand.CreateFromTask<string>(OpenExplorer);
             SearchJava = ReactiveCommand.CreateFromTask(async () =>
             {
                 await Dispatcher.UIThread.InvokeAsync(() => AppState.ITAUnits.Notify(0, "正在搜索Java", "请耐心等待......"));
@@ -28,10 +30,9 @@ namespace LSL.ViewModels
             CheckUpdateCmd = ReactiveCommand.CreateFromTask(serveCon.CheckForUpdates);
         }
 
-        #region 泛公共操作
+        #region 打开网页命令
         public ICommand OpenWebPageCmd { get; }
-        public ICommand CheckUpdateCmd { get; }
-        private async void OpenWebPage(string url)
+        private async Task OpenWebPage(string url)
         {
             try
             {
@@ -46,20 +47,20 @@ namespace LSL.ViewModels
                 AppState.ITAUnits.Notify(1, "成功打开了网页！", url);
                 _logger.LogInformation("Successfully opened web page {url}.", url);
             }
-            catch (Win32Exception noBrowser)
-            {
-                if (noBrowser.ErrorCode == -2147467259)
-                {
-                    await AppState.ITAUnits.PopupITA.Handle(new InvokePopupArgs(PopupType.Error_Confirm, "打开网页失败",
-                        $"LSL未能成功打开网页{url}，请检查您的系统是否设置了默认浏览器。\r错误内容：{noBrowser.Message}"));
-                    _logger.LogError(noBrowser, "Error opening webpage {url} because no default web browser is set.",
-                        url);
-                }
-            }
             catch (ArgumentException ae)
             {
                 _logger.LogError(ae, "Error opening webpage {url} because of invalid URL format.", url);
                 await AppState.ITAUnits.ThrowError("打开网页失败", $"URL格式不正确：{url}");
+            }
+            catch (Win32Exception noBrowser)
+            {
+                if (noBrowser.ErrorCode == -2147467259)
+                {
+                    await AppState.ITAUnits.ThrowError("打开网页失败",
+                        $"LSL未能成功打开网页{url}，请检查您的系统是否设置了默认浏览器。\r错误内容：{noBrowser.Message}");
+                    _logger.LogError(noBrowser, "Error opening webpage {url} because no default web browser is set.",
+                        url);
+                }
             }
             catch (Exception ex)
             {
@@ -68,12 +69,70 @@ namespace LSL.ViewModels
                     : "Please check MacOS's default web browser configuration.";
                 _logger.LogError(ex, "Error opening webpage {url}.\r{logMsg}", url, logMsg);
                 var uiMsg = OperatingSystem.IsLinux()
-                    ? "请安装 xdg-utils: sudo apt install xdg-utils"
-                    : "macOS系统异常，请检查默认浏览器设置";
+                    ? "请确保安装了 xdg-utils 并且设置了默认浏览器以使用打开浏览器网址的功能。"
+                    : "在此MacOS系统上似乎无法正常打开网页，请检查默认浏览器设置。";
                 await AppState.ITAUnits.ThrowError("打开网页失败", uiMsg + ex.Message);
             }
         }
+
         #endregion
+        
+        #region 打开文件（夹）命令
+        public ICommand OpenFileCmd { get; }
+        private async Task OpenExplorer(string url)
+        {
+            try
+            {
+                //classify
+                bool isFile;
+                if (Path.GetPathRoot(url) == url) isFile = false;
+                else if (File.Exists(url)) isFile = true;
+                else if (Directory.Exists(url)) isFile = false;
+                else throw new ArgumentException("File or directory not found.", nameof(url));
+                //execute
+                if (isFile) OpenAndSelectFile(url);
+                else OpenDir(url);
+            }
+            catch (ArgumentException ae)
+            {
+                _logger.LogError(ae, "File or directory not found when trying to open file explorer.");
+                await AppState.ITAUnits.ThrowError("打开文件失败", $"不存在位于{url}的文件或目录。");
+            }
+            catch (DirectoryNotFoundException dnfe)
+            {
+                _logger.LogError(dnfe, "Parent directory not found when trying to open file explorer.");
+                await AppState.ITAUnits.ThrowError("打开文件失败", $"无法获取位于{url}的文件的父目录。");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error executing open explorer task.");
+                await AppState.ITAUnits.ThrowError("打开文件失败", $"在文件资源管理器中打开{url}时出现以下报错：\r{e.Message}");
+            }
+        }
+
+        private static void OpenDir(string url)
+        {
+            if (OperatingSystem.IsWindows())
+                Process.Start("explorer.exe", [url]);
+            else if (OperatingSystem.IsLinux())
+                Process.Start("xdg-open", [url]); // xdg-utils dependency required
+            else if (OperatingSystem.IsMacOS())
+                Process.Start("open", [url]);
+        }
+
+        private static void OpenAndSelectFile(string url)
+        {
+            var parent = Directory.GetParent(url);
+            if (parent is null) throw new DirectoryNotFoundException($"Cannot get the parent directory of file {url}.");
+            if (OperatingSystem.IsWindows())
+                Process.Start("explorer.exe", ["/select,", url]);
+            else if (OperatingSystem.IsLinux())
+                Process.Start("xdg-open", [parent.FullName]); // xdg-utils dependency required
+            else if (OperatingSystem.IsMacOS())
+                Process.Start("open", ["-R", url]);
+        }
+        #endregion
+        public ICommand CheckUpdateCmd { get; }
 
         public ICommand SearchJava { get; }
     }
