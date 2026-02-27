47daecac977a7ac6004ab056e9c0ee6de85cce60
orange-icepop <czq13482103069@hotmail.com>
2026-02-27 10:37:11 +0800

    修复Jvm数据显示问题

--- LSL/Views/Server/ServerConf.axaml.cs
+++ LSL/Views/Server/ServerConf.axaml.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
 
 namespace LSL.Views.Server;
 
@@ -7,5 +13,35 @@ public partial class ServerConf : UserControl
     public ServerConf()
     {
         InitializeComponent();
+    }
+}
+
+public class JvmParamConverter : IValueConverter
+{
+    public static readonly JvmParamConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, 
+        CultureInfo culture)
+    {
+        if (value is IEnumerable<string> source && targetType.IsAssignableTo(typeof(string)))
+        {
+            return string.Join(' ', source);
+        }
+        // converter used for the wrong type
+        return new BindingNotification(new InvalidCastException(), 
+            BindingErrorType.Error);
+    }
+
+    public object ConvertBack(object? value, Type targetType, 
+        object? parameter, CultureInfo culture)
+    {
+        if (value is string s)
+        {
+            return s.Split(
+                ["\r\n", "\r", "\n"],
+                StringSplitOptions.RemoveEmptyEntries
+            ).Select(line => line.Trim()).ToList();
+        }
+        throw new InvalidCastException();
     }
 }
